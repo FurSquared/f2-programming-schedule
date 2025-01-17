dd6c0cccb24b49a6d6cb634b1a4039ae9d150452
Phillip Pollard <phil@crescendo.net>
2025-01-16 20:03:34 -0500

    Apparently we start at 8:00am in the game room.

--- verify.pl
+++ verify.pl
@@ -19,12 +19,12 @@ use strict;
 ### Read the panel list
 
 my %dittman_time_code;
-map {$dittman_time_code{$_} = 'A'} ('9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM');
+map {$dittman_time_code{$_} = 'A'} ('8:00 AM', '8:30 AM', '9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM');
 map {$dittman_time_code{$_} = 'B'} ('1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM');
 map {$dittman_time_code{$_} = 'C'} ('5:00 PM', '5:30 PM', '6:00 PM', '6:30 PM', '7:00 PM', '7:30 PM', '8:00 PM', '8:30 PM');
 map {$dittman_time_code{$_} = 'D'} ('9:00 PM', '9:30 PM', '10:00 PM', '10:30 PM', '11:00 PM', '11:30 PM', '12:00 AM', '12:30 AM', '1:00 AM', '1:30 AM');
 
-my @times = ('9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM',
+my @times = ('8:00 AM', '8:30 AM', '9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM',
 	'1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM', '5:00 PM', '5:30 PM',
     '6:00 PM', '6:30 PM', '7:00 PM', '7:30 PM', '8:00 PM', '8:30 PM', '9:00 PM', '9:30 PM', '10:00 PM', '10:30 PM',
     '11:00 PM', '11:30 PM', '12:00 AM', '12:30 AM', '1:00 AM', '1:30 AM');
@@ -209,7 +209,7 @@ for my $day (keys %data) {
 			# Check: panelist time
 	
 			my $time_code = $dittman_time_code{$time};
-			die "Bad time code" unless $time_code;
+			die "Bad time code ($time)" unless $time_code;
 			my $time_pref = $panels{$scheduled_panel}{'pref'}{$day};
 			my $time_avail = $panels{$scheduled_panel}{'avail'}{$day};
 			if ( $length =~ /SPECIAL TIME/ ) {
